f2c896805c6754cca5e1ce14047ea7445af9fd2c
wildman9x <wildmanyt@gmail.com>
2024-03-29 01:18:45 +0700

    bruh

---
+++ DotnetGRPCClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Collections.Generic;
+using DotnetGRPC;
+
+Console.WriteLine("Hello World!");
+
+using var channel = GrpcChannel.ForAddress("https://comp1640api.azurewebsites.net:8585");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(
+    new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
