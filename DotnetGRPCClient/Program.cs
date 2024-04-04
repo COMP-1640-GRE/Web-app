be50d04bedf8a9d1a50fc1cfc07fc9060d43a340
wildman9x <wildmanyt@gmail.com>
2024-04-04 20:42:52 +0700

    Client to test zippin files

--- DotnetGRPCClient/Program.cs
+++ DotnetGRPCClient/Program.cs
@@ -2,14 +2,32 @@
 using System;
 using System.Collections.Generic;
 using DotnetGRPC;
+using System.IO;
+using Google.Protobuf;
 
 Console.WriteLine("Hello World!");
 
 using var channel = GrpcChannel.ForAddress("https://comp1640api.azurewebsites.net");
-var client = new Greeter.GreeterClient(channel);
+var client = new FileTransfer.FileTransferClient(channel);
 
-var reply = await client.SayHelloAsync(
-    new HelloRequest { Name = "GreeterClient" });
-Console.WriteLine("Greeting: " + reply.Message);
+var reply = client.DownloadMultipleFileAsZip(
+    new DownloadMultipleFileAsZipRequest
+    {
+        Filename = "file",
+        Urls =
+        {"https://comp1640-blob.sgp1.digitaloceanspaces.com/comp1640-blob/path/to/files/file1.txt",
+        "https://comp1640-blob.sgp1.digitaloceanspaces.com/comp1640-blob/path/to/files/file2.txt",
+        "https://comp1640-blob.sgp1.cdn.digitaloceanspaces.com/comp1640-blob/123/comp1640ssh.pem"
+        }
+    });
+// Assuming you have a ByteString object
+ByteString byteString = reply.Zip;
+
+// Convert ByteString to byte array
+byte[] bytes = byteString.ToByteArray();
+
+// Write byte array to a file
+File.WriteAllBytes(reply.Name, bytes);
+Console.WriteLine($"File downloaded as {reply.Name}");
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
