f27c1878c163da5c3325deefa3cfe8b507ca11a6
Anh Trinh <wildmanyt@gmail.com>
2024-04-05 15:54:42 +0700

    hehe

--- DotnetGRPCClient/Program.cs
+++ DotnetGRPCClient/Program.cs
@@ -10,24 +10,36 @@ Console.WriteLine("Hello World!");
 using var channel = GrpcChannel.ForAddress("https://comp1640api.azurewebsites.net");
 var client = new FileTransfer.FileTransferClient(channel);
 
-var reply = client.DownloadMultipleFileAsZip(
-    new DownloadMultipleFileAsZipRequest
+var request = new DownloadMultipleFileAsZipRequest
+{
+    Filename = "file",
+    Urls =
     {
-        Filename = "file",
-        Urls =
-        {"https://comp1640-blob.sgp1.digitaloceanspaces.com/comp1640-blob/path/to/files/file1.txt",
+        "https://comp1640-blob.sgp1.digitaloceanspaces.com/comp1640-blob/path/to/files/file1.txt",
         "https://comp1640-blob.sgp1.digitaloceanspaces.com/comp1640-blob/path/to/files/file2.txt",
         "https://comp1640-blob.sgp1.cdn.digitaloceanspaces.com/comp1640-blob/123/comp1640ssh.pem"
-        }
-    });
-// Assuming you have a ByteString object
-ByteString byteString = reply.Zip;
+    }
+};
 
-// Convert ByteString to byte array
-byte[] bytes = byteString.ToByteArray();
+using var call = client.DownloadMultipleFileAsZip(request);
 
-// Write byte array to a file
-File.WriteAllBytes(reply.Name, bytes);
-Console.WriteLine($"File downloaded as {reply.Name}");
+long totalBytesRead = 0;
+long totalBytes = 0;
+await foreach (var response in call.ResponseStream.ReadAllAsync())
+{
+    totalBytesRead += response.Zip.Length;
+    totalBytes = response.Size;
+    double percent = (double)totalBytesRead / totalBytes * 100;
+    Console.WriteLine($"Downloaded {percent}%");
+
+    // Write the chunk to a file
+    using (var stream = new FileStream(response.Filename, FileMode.Append))
+    {
+        var bytes = response.Zip.ToByteArray();
+        await stream.WriteAsync(bytes, 0, bytes.Length);
+    }
+}
+
+Console.WriteLine($"File downloaded as {request.Filename}");
 Console.WriteLine("Press any key to exit...");
 Console.ReadKey();
