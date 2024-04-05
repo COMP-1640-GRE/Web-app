8bac29c8e1aef5bb3f03e551f1ef91535afd9b71
Anh Trinh <wildmanyt@gmail.com>
2024-04-05 13:46:41 +0700

    fix csproj

---
+++ DotnetGRPC/Services/NotificationService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetGRPC.Services
+{
+    public class NotificationService : Notification.NotificationBase
+    {
+        
+    }
+}
