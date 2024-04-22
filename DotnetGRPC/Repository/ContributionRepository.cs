379ac50fac35dd2ebc5c7084dc446cd886622f6b
cuong <cuongph20@fpt.com.vn>
2024-04-22 23:59:28 +0700

    update report

--- DotnetGRPC/Repository/ContributionRepository.cs
+++ DotnetGRPC/Repository/ContributionRepository.cs
@@ -1,4 +1,5 @@
 using DotnetGRPC.Model;
+using DotnetGRPC.Model.DTO;
 using Microsoft.EntityFrameworkCore;
 
 public class ContributionRepository
@@ -19,4 +20,75 @@ public class ContributionRepository
             .ToListAsync();
     }
 
+    public async Task<Dictionary<string, int>> CountContributionsByFaculty()
+    {
+        var contributionsWithFaculty = _context.Contribution
+            .Join(_context.User,
+                c => c.DbAuthorId,
+                u => u.Id,
+                (c, u) => new { Contribution = c, User = u })
+            .Join(_context.Faculty,
+                cu => cu.User.FacultyId,
+                f => f.Id,
+                (cu, f) => new { cu.Contribution, cu.User, Faculty = f });
+
+        return await contributionsWithFaculty
+            .GroupBy(c => c.Faculty.Name)
+            .Select(g => new { Faculty = g.Key, Count = g.Count() })
+            .ToDictionaryAsync(x => x.Faculty, x => x.Count);
+    }
+
+    public async Task<Dictionary<string, int>> CountContributorsByFaculty()
+    {
+        var contributorsWithFaculty = _context.Contribution
+            .Join(_context.User,
+                c => c.DbAuthorId,
+                u => u.Id,
+                (c, u) => new { Contribution = c, User = u })
+            .Join(_context.Faculty,
+                cu => cu.User.FacultyId,
+                f => f.Id,
+                (cu, f) => new { cu.Contribution, cu.User, Faculty = f });
+
+        return await contributorsWithFaculty
+            .GroupBy(c => c.Faculty.Name)
+            .Select(g => new { Faculty = g.Key, Count = g.Select(x => x.User.Id).Distinct().Count() })
+            .ToDictionaryAsync(x => x.Faculty, x => x.Count);
+    }
+
+    public async Task<Dictionary<string, double>> AverageContributionsByFaculty()
+    {
+        var contributionsWithFaculty = _context.Contribution
+            .Join(_context.User,
+                c => c.DbAuthorId,
+                u => u.Id,
+                (c, u) => new { Contribution = c, User = u })
+            .Join(_context.Faculty,
+                cu => cu.User.FacultyId,
+                f => f.Id,
+                (cu, f) => new { cu.Contribution, cu.User, Faculty = f });
+
+        return await contributionsWithFaculty
+            .GroupBy(c => c.Faculty.Name)
+            .Select(g => new { Faculty = g.Key, Average = g.Count() / (double)g.Select(x => x.User.Id).Distinct().Count() })
+            .ToDictionaryAsync(x => x.Faculty, x => x.Average);
+    }
+
+    public async Task<List<ContributionTrend>> ContributionTrendsByFaculty()
+    {
+        var contributionsWithFaculty = _context.Contribution
+            .Join(_context.User,
+                c => c.DbAuthorId,
+                u => u.Id,
+                (c, u) => new { Contribution = c, User = u })
+            .Join(_context.Faculty,
+                cu => cu.User.FacultyId,
+                f => f.Id,
+                (cu, f) => new { cu.Contribution, cu.User, Faculty = f });
+
+        return await contributionsWithFaculty
+            .GroupBy(c => new { c.Faculty.Name, Year = c.Contribution.UpdatedAt.Year, Month = c.Contribution.UpdatedAt.Month })
+            .Select(g => new ContributionTrend { Faculty = g.Key.Name, Date = $"{g.Key.Year}-{g.Key.Month}", Count = g.Count() })
+            .ToListAsync();
+    }
 }
